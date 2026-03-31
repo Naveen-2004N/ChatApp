c4260f3389d1e1f71ead33802964cdbc3fa1c14c
Naveen <navinmachari77@gmail.com>
2026-03-31 19:24:08 +0530

    Initial Commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" 
+    ? "http://localhost:3000/api" 
+    : "https://chatapp-backend-xh7o.onrender.com/api",
   withCredentials: true,
 });
