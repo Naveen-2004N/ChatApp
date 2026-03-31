522df3ce1176510f35fba306f8468468bdfe4546
Naveen <navinmachari77@gmail.com>
2026-03-31 22:27:30 +0530

    Initial Commit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,14 +7,14 @@ export const generateToken = (userId, res) => {
     throw new Error("JWT_SECRET is not configured");
   }
 
-  const token = jwt.sign({ userId }, JWT_SECRET, {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
+    sameSite: "none", // CSRF attacks
     secure: ENV.NODE_ENV === "development" ? false : true,
   });
 
